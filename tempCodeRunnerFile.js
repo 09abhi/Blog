74f42743e70263843d1e540e5f67245bc6fd4799
09abhi <96674255+09abhi@users.noreply.github.com>
2022-09-05 13:23:16 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+app.get("/posts/:postId", function(req, res){
+
+const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
+});
